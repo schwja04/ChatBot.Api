06d1f261b9fce0d54b448971b0aa938cd7981450
Jacob Schwartz <schwja04@gmail.com>
2024-06-11 23:22:04 -0500

    Adding a appsettings.Local.json to test configuration

--- src/ChatBot.Api/Program.cs
+++ src/ChatBot.Api/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Local"))
 {
     app.UseSwagger();
     app.UseSwaggerUI();
