eb6a9bfd441d6947b366031fa774234ef1a47385
Jacob Schwartz <schwja04@gmail.com>
2025-01-22 12:32:31 -0600

    Move Common.HttpClient default configuration to leave space for service discovery

--- common/Common.HttpClient/Models/HttpClientConfiguration.cs
+++ common/Common.HttpClient/Models/HttpClientConfiguration.cs
@@ -2,21 +2,42 @@ using Microsoft.Extensions.Configuration;
 
 namespace Common.HttpClient.Models;
 
-public class HttpClientConfiguration<TImplementationType>(IConfiguration configuration)
-    : HttpClientConfiguration(typeof(TImplementationType).Name, configuration);
+public class HttpClientConfiguration<TImplementationType> : HttpClientConfiguration
+{
+    public HttpClientConfiguration(IConfiguration configuration) : base(typeof(TImplementationType).Name, configuration)
+    {
+    }
+
+    public HttpClientConfiguration(IConfigurationSection configurationSection) 
+        : base(typeof(TImplementationType).Name, configurationSection)
+    {
+    }
+}
 
 public class HttpClientConfiguration
 {
     protected HttpClientConfiguration(string clientTypeName, IConfiguration configuration)
     {
-        var clientSection = configuration.GetSection($"Services:{clientTypeName}");
+        var clientSection = configuration.GetSection($"ServiceClients:{clientTypeName}");
         if (!clientSection.Exists())
         {
-            throw new ArgumentNullException(clientTypeName, $"No child configuration section found in 'Services' for {clientTypeName}.");
+            throw new ArgumentNullException(clientTypeName, $"No child configuration section found in 'ServiceClients' for {clientTypeName}.");
         }
 
         Uri? baseAddress = clientSection.GetValue<Uri>("BaseAddress");
-        if (baseAddress is not null && !baseAddress.AbsoluteUri.EndsWith("/"))
+        if (baseAddress is not null && !baseAddress.AbsoluteUri.EndsWith('/'))
+        {
+            baseAddress = new Uri($"{baseAddress.AbsoluteUri}/");
+        }
+        BaseAddress = baseAddress!;
+
+        ClientName = clientTypeName;
+    }
+    
+    protected HttpClientConfiguration(string clientTypeName, IConfigurationSection configurationSection)
+    {
+        Uri? baseAddress = configurationSection.GetValue<Uri>("BaseAddress");
+        if (baseAddress is not null && !baseAddress.AbsoluteUri.EndsWith('/'))
         {
             baseAddress = new Uri($"{baseAddress.AbsoluteUri}/");
         }
