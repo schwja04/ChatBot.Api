13e9ee203705a2dab5a45dbbb5c8e8f2b2fd3d69
Jacob Schwartz <schwja04@gmail.com>
2025-01-22 21:53:45 -0600

    Let Aspire fill in more of the environment

--- aspire/ChatBot.Aspire.AppHost/Program.cs
+++ aspire/ChatBot.Aspire.AppHost/Program.cs
@@ -3,11 +3,12 @@ using Microsoft.Extensions.Configuration;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var mongoConfig = builder.Configuration.GetRequiredSection("Mongo");
-var mongoUsername = mongoConfig["Username"];
-var mongoPassword = mongoConfig["Password"];
+var mongoUsername = mongoConfig["Username"]!;
+var mongoPassword = mongoConfig["Password"]!;
+var databaseName = mongoConfig["DatabaseName"]!;
 
-var username = builder.AddParameter("username", mongoUsername!);
-var password = builder.AddParameter("password", mongoPassword!);
+var username = builder.AddParameter("username", mongoUsername);
+var password = builder.AddParameter("password", mongoPassword);
 
 var mongo = builder
     .AddMongoDB(
@@ -18,13 +19,18 @@ var mongo = builder
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent)
     .WithMongoExpress();
-var mongoDb = mongo.AddDatabase("chatbot");
+var mongoDb = mongo.AddDatabase(databaseName);
+
+var openAIServiceConfig = builder.Configuration.GetRequiredSection("Services:OpenAIService");
+var openAIServiceHttp = openAIServiceConfig["http"]!;
 
 builder.AddProject<Projects.ChatBot_Api>("chatbot-api")
     .WithReference(mongoDb)
     .WaitFor(mongoDb)
     .WithEnvironment("Mongo__ConnectionString", mongoDb.Resource.ConnectionStringExpression)
     .WithEnvironment("Mongo__Username", username.Resource.Value)
-    .WithEnvironment("Mongo__Password", password.Resource.Value);
+    .WithEnvironment("Mongo__Password", password.Resource.Value)
+    .WithEnvironment("Mongo__DatabaseName", databaseName)
+    .WithEnvironment("Services__OpenAIService__http", openAIServiceHttp);
 
 builder.Build().Run();
