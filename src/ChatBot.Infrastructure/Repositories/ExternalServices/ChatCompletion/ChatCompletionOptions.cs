534a69630ea6e428ff528201b31eac6da6708988
Jacob Schwartz <schwja04@gmail.com>
2025-03-08 20:56:08 -0600

    Move to IChatClient

--- src/ChatBot.Infrastructure/Repositories/ExternalServices/ChatCompletion/ChatCompletionOptions.cs
+++ src/ChatBot.Infrastructure/Repositories/ExternalServices/ChatCompletion/ChatCompletionOptions.cs
@@ -2,5 +2,10 @@ namespace ChatBot.Infrastructure.Repositories.ExternalServices.ChatCompletion;
 
 internal record ChatCompletionOptions
 {
-    public required string Model { get; init; }
+    public required string DefaultModel { get; init; } = "llama3.2";
+    public required float DefaultFrequencyPenalty { get; init; } = 0.0F;
+    public required int DefaultMaxTokens { get; init; } = 100;
+    public required float DefaultPresencePenalty { get; init; } = 0.0F;
+    public required float DefaultTemperature { get; init; } = 1.0F;
+    public required float DefaultTopP { get; init; } = 1.0F;
 }
