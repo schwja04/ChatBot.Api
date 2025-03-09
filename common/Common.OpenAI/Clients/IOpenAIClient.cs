9e62fcb7f30e8928b4417a90501b4e50407482ff
Jacob Schwartz <schwja04@gmail.com>
2025-03-08 20:58:01 -0600

    Remove old implementation

--- common/Common.OpenAI/Clients/IOpenAIClient.cs
+++
@@ -1,8 +0,0 @@
-using Common.OpenAI.Models;
-
-namespace Common.OpenAI.Clients;
-
-public interface IOpenAIClient
-{
-    Task<CreateChatCompletionResponse> CreateChatCompletionAsync(CreateChatCompletionRequest request, CancellationToken cancellationToken = default);
-}
